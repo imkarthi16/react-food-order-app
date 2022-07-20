29e9a3b11cdb01f0f0e9f08caa93753d67c65852
imkarthi16 <karthiveera@gmail.com>
2022-07-20 12:30:01 +0530

    Adding a from

--- src/components/Meals/AvaliableMeals.js
+++ src/components/Meals/AvaliableMeals.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Card from "../UI/Card";
 import classes from "./AvaliableMeals.module.css";
-
+import MealItem from "./MealItem/MealItem";
 
 const DUMMY_MEALS = [
   {
@@ -31,7 +31,7 @@ const DUMMY_MEALS = [
 ];
 
 const AvaliableMeals = () => {
-  const mealList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealList = DUMMY_MEALS.map((meal) => <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price} />);
   return (
     <section className={classes.meals}>
       <Card>
